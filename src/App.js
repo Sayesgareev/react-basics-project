5f5fab29a44d65838ed0ee5cb4e922fd5e1a70b4
Sayesgareev <116744433+Sayesgareev@users.noreply.github.com>
2023-01-22 20:22:51 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Review from "./Review";
+
+function App() {
+  return (
+    <div className='App'>
+      <main>
+        <section className='container'>
+          <div className='title'>
+            <h2>our reviews</h2>
+            <div className='underline'></div>
+          </div>
+          <Review />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Prohorenko Roman",
+    job: "train driver",
+    image:
+      "https://sun9-76.userapi.com/impg/zLotb6fkvE_-Rt_ZAi-XHnl96dgL2wa_OOu4Kw/lneZ3nU67ZI.jpg?size=813x1080&quality=95&sign=1a3fb2225b64c22dc2beff1b364d477a&type=album",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate vel est et consequat. Aliquam suscipit volutpat eros eu dapibus. Proin imperdiet sit amet ligula at auctor..",
+  },
+  {
+    id: 2,
+    name: "Andrew Kirilenko",
+    job: "medic",
+    image:
+      "https://sun9-45.userapi.com/impg/jynbili4vGVxinu1eZNyNbZWCzFq9WF-O8kMdg/ZmuivJ1R5O0.jpg?size=808x1080&quality=96&sign=671903b5d8d38c628dc76aec38e2b36e&type=album",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate vel est et consequat. Aliquam suscipit volutpat eros eu dapibus. Proin imperdiet sit amet ligula at auctor..",
+  },
+  {
+    id: 3,
+    name: "Mattew Popov",
+    job: "engineer",
+    image:
+      "https://sun9-82.userapi.com/impf/Knv-tHHuSEtq5ncZfnZCISeiZIFL-lCHxCl5LA/uM-LuPkGscw.jpg?size=1280x851&quality=96&sign=f160adb5d3719311bc7d7388bd97e8de&type=album",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate vel est et consequat. Aliquam suscipit volutpat eros eu dapibus. Proin imperdiet sit amet ligula at auctor.",
+  },
+
+  {
+    id: 4,
+    name: "Kirill Gurin",
+    job: "worker",
+    image:
+      "https://sun9-54.userapi.com/impg/aobugzHzbahd6BEgM-nbcJcalDV0PkQdgQvF2A/ZlkITg_cpKo.jpg?size=810x1080&quality=96&sign=4a696d53d6c699754595ac0b053a5794&type=album",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate vel est et consequat. Aliquam suscipit volutpat eros eu dapibus. Proin imperdiet sit amet ligula at auctor.",
+  },
+  {
+    id: 5,
+    name: "Alexey Dobaev",
+    job: "worker",
+    image:
+      "https://sun9-46.userapi.com/impg/B8gU_lzLNzcghVlYWMIwh2DunBcuEMCGyHs_nA/-UXlzgDBjio.jpg?size=810x1080&quality=96&sign=f2c9002655dec43789180f7d03f6ec81&type=album",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate vel est et consequat. Aliquam suscipit volutpat eros eu dapibus. Proin imperdiet sit amet ligula at auctor.",
+  },
+];
+
+export default reviews;
